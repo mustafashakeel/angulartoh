0cc21076d19439c2a82a3db219a3e1a8178f5fad
Mustafa Qureshi <mustafashakeel@gmail.com>
2021-11-16 11:59:24 -0800

    initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,8 @@
+import { Hero } from './hero';
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Batman' },
+  { id: 2, name: 'Superman' },
+  { id: 3, name: 'Spiderman' },
+  { id: 4, name: 'Flash' },
+  { id: 6, name: 'Aquaman' },
+]
