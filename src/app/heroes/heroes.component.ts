6a0967af5b03d9bd0ee958dce4aff9b71467aeed
Mustafa Qureshi <mustafashakeel@gmail.com>
2021-11-16 14:46:36 -0800

    added rxjs to service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -20,7 +20,11 @@ export class HeroesComponent implements OnInit {
 
   }
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().subscribe((heroes) => {
+
+      return this.heroes = heroes;
+
+    });
 
   }
 
